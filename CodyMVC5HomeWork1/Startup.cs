c72f4ac5a43f9cd4b7946a914189695052fbe476
codyidle <cody@edetw.com>
2016-03-21 18:47:24 +0800

    剛建立第一次簽入

---
+++ CodyMVC5HomeWork1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodyMVC5HomeWork1.Startup))]
+namespace CodyMVC5HomeWork1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
