2c8bd376cc428674817b35405e8cec5d5faa86ce
codyidle <cody@edetw.com>
2016-03-24 00:19:58 +0800

    完成資料數清單頁面簽入

---
+++ CodyMVC5HomeWork1/Controllers/客戶資料數清單Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodyMVC5HomeWork1.Models;
+
+namespace CodyMVC5HomeWork1.Controllers
+{
+
+    public class 客戶資料數清單Controller : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+        // GET: 客戶資料數清單
+        public ActionResult Index()
+        {
+            return View(db.客戶資料數清單.ToList());
+        }
+    }
+}
